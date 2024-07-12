0892d1dcf4f50705f1a0e1037587599f6ea98ea5
Shreyash Badole <shreyashbadole29031998@gmail.com>
2024-07-12 11:32:31 +0530

    bug fix try2

--- webtolead/script.js
+++ webtolead/script.js
@@ -1,30 +1,29 @@
-console.log("script inputed");
-
+console.log("script loaded");
 let captchachecked = false;
-
 function beforesubmit(event) {
     if (captchachecked) {
         let outputdate = document.querySelector(".outputdate");
         let inputdate = document.querySelector(".inputdate");
         console.log("check date:", inputdate.value);
-
         // Convert input date to a formatted date (en-IN locale)
         let formateddate = new Date(inputdate.value).toLocaleDateString("en-IN");
         outputdate.value = formateddate;
-
         console.log("output date:", outputdate.value);
     } else {
-        
         alert("Please check the captcha box to generate the lead");
-        event.preventDefault(); 
-       
+        event.preventDefault();
     }
 }
-
-function timestamp() { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500); 
+function timestamp() {
+    var response = document.getElementById("g-recaptcha-response");
+    if (response == null || response.value.trim() == "") {
+        var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
+        elems["ts"] = JSON.stringify(new Date().getTime());
+        document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems);
+    }
+}
 setInterval(timestamp, 500);
-
-
-function recapcthasuccess() {
+function recaptchaSuccess() {
+    console.log("reCAPTCHA verified");
     captchachecked = true;
 }
